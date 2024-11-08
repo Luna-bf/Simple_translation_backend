a3643f1556875f2605334338ba0eea114b2ec344
Luna-bf <lburattifayolle@gmail.com>
2024-11-08 16:34:28 +0100

    Create es.php

---
+++ languages/es.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang = [
+    'Accueil' => 'Inicio',
+    'Compétences' => 'Habilidades',
+    'Formations' => 'Formacións',
+    'Projets' => 'Proyectos',
+    'Contact' => 'Contacto',
+    'Bonjour, John' => 'Holà, John',
+    'Profil' => 'Perfil',
+    'Paramètres' => 'Ajustes',
+    'Se déconnecter' => 'Finalizar la sesión',
+    'Langue' => 'Idioma',
+    'Celeste, c\'est quoi ?' => '¿Qué es Celeste?',
+    'Celeste est un jeu de plateforme acclamé par la critique créé par Extremely OK Games. Le jeu est sorti le 25 janvier 2018 sur Nintendo Switch, PlayStation 4, Xbox One, PC, Mac et Linux. Le jeu suit une jeune femme nommée Madeline alors qu\'elle escalade la montagne quelque peu magique de Celeste, faisant face à l\'opposition des obstacles naturels de la montagne et d\'elle-même.' => 'Celeste es un juego de plataformas aclamado por la crítica creado por Extremely OK Games. El juego fue lanzado el 25 de enero de 2018 para Nintendo Switch, PlayStation 4, Xbox One, PC, Mac y Linux. El juego sigue a una joven llamada Madeline mientras escala la algo mágica Montaña Celeste, enfrentándose a la oposición de los obstáculos naturales de la montaña y de ella misma.',
+    'Gameplay' => 'Jugabilidad',
+    'Le gameplay de Celeste propose des commandes rapides et précises le long d\'une série de sous-niveaux de plateformes difficiles qui constituent un chapitre plus vaste . Il suit les commandes simples à apprendre et difficiles à maîtriser qui sont un élément de base de son genre, et le gameplay a souvent été comparé à d\'autres, comme Super Meat Boy.' => 'La jugabilidad de Celeste presenta controles rápidos y precisos a lo largo de una serie de desafiantes subniveles de plataformas que conforman un capítulo más grande. Sigue los controles fáciles de aprender y difíciles de dominar que son un elemento básico de su género, y la jugabilidad a menudo se ha comparado con otros, como Super Meat Boy.',
+    'Le prologue présente aux joueurs les commandes de base, comme le saut et la capacité à grimper aux murs. Le tiret, l\'une des principales options de mouvement du jeu, est offert au joueur à la fin du niveau, apparemment par l\'Oiseau . Ce tiret est la mécanique de base du gameplay de Celeste, offrant au joueur une courte poussée vers l\'avant dans la direction que le joueur tient. Il ne peut être utilisé qu\'un nombre limité de fois et doit être rafraîchi avant de se lancer à nouveau. L\'état des tirets disponibles du joueur est indiqué par la couleur des cheveux de Madeline. Le rafraîchissement du tiret se fait de multiples façons, mais les principales actions incluent toucher le sol, collecter des cristaux d\'endurance et les transitions d\'écran.' => 'El prólogo presenta a los jugadores controles básicos, como saltar y la capacidad de escalar paredes. El guión, una de las principales opciones de movimiento del juego, se ofrece al jugador al final del nivel, aparentemente por el Pájaro. Esta carrera es la mecánica principal del juego de Celeste, que le proporciona al jugador un breve empujón hacia adelante en la dirección que sostiene. Solo se puede utilizar un número limitado de veces y se debe actualizar antes de volver a iniciarlo. El estado del tablero disponible del jugador se indica por el color del cabello de Madeline. La actualización del tablero se realiza de varias maneras, pero las acciones principales incluyen tocar el suelo, recolectar cristales de resistencia y transiciones de pantalla.',
+    'Les sous-niveaux, et par extension les chapitres du jeu, deviennent progressivement plus difficiles à mesure que de nouvelles mécaniques et obstacles sont introduits, et l\'utilisation de ces éléments fusionne et s\'harmonise à mesure que le joueur se rapproche du sommet. Vers la fin du jeu, Celeste introduit de nombreux autres mouvements qui utilisent diverses combinaisons des trois mouvements de base (sauter, grimper et courir) en tandem; ces techniques sont explorées en détail sur la page Moves.' => 'Los subniveles, y por extensión los capítulos del juego, se vuelven progresivamente más difíciles a medida que se introducen nuevas mecánicas y obstáculos, y el uso de estos elementos se fusiona y armoniza a medida que el jugador se acerca a la cima. Hacia el final del juego, Celeste introduce muchos otros movimientos que utilizan varias combinaciones de los tres movimientos básicos (saltar, trepar y correr) en conjunto; Estas técnicas se exploran en detalle en la página de Movimientos.',
+];
+
+?>
