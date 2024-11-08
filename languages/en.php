d4b0d443ea0370f2d8ff3ebeb7d168ea87528226
Luna-bf <lburattifayolle@gmail.com>
2024-11-08 16:34:06 +0100

    Create en.php

---
+++ languages/en.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang = [
+    'Accueil' => 'Home',
+    'Compétences' => 'Skills',
+    'Formations' => 'Trainings',
+    'Projets' => 'Projects',
+    'Contact' => 'Contact',
+    'Bonjour, John' => 'Hi, John',
+    'Profil' => 'Profile',
+    'Paramètres' => 'Settings',
+    'Se déconnecter' => 'Logout',
+    'Langue' => 'Language',
+    'Celeste, c\'est quoi ?' => 'What is Celeste ?',
+    'Celeste est un jeu de plateforme acclamé par la critique créé par Extremely OK Games. Le jeu est sorti le 25 janvier 2018 sur Nintendo Switch, PlayStation 4, Xbox One, PC, Mac et Linux. Le jeu suit une jeune femme nommée Madeline alors qu\'elle escalade la montagne quelque peu magique de Celeste, faisant face à l\'opposition des obstacles naturels de la montagne et d\'elle-même.' => 'Celeste is a critically acclaimed platform game created by Extremely OK Games. The game was released on January 25, 2018, for Nintendo Switch, PlayStation 4, Xbox One, PC, Mac, and Linux. The game follows a young woman named Madeline as she climbs the somewhat magical Celeste Mountain, facing opposition from both the mountain\'s natural obstacles and from within herself.',
+    'Gameplay' => 'Gameplay',
+    'Le gameplay de Celeste propose des commandes rapides et précises le long d\'une série de sous-niveaux de plateformes difficiles qui constituent un chapitre plus vaste . Il suit les commandes simples à apprendre et difficiles à maîtriser qui sont un élément de base de son genre, et le gameplay a souvent été comparé à d\'autres, comme Super Meat Boy.' => 'Celeste\'s gameplay features fast and tight controls along a series of challenging platforming sublevels that make up a larger Chapter. It follows the simple to learn and hard to master controls that are a staple of its genre, and gameplay has often been compared to others, like Super Meat Boy.',
+    'Le prologue présente aux joueurs les commandes de base, comme le saut et la capacité à grimper aux murs. Le tiret, l\'une des principales options de mouvement du jeu, est offert au joueur à la fin du niveau, apparemment par l\'Oiseau . Ce tiret est la mécanique de base du gameplay de Celeste, offrant au joueur une courte poussée vers l\'avant dans la direction que le joueur tient. Il ne peut être utilisé qu\'un nombre limité de fois et doit être rafraîchi avant de se lancer à nouveau. L\'état des tirets disponibles du joueur est indiqué par la couleur des cheveux de Madeline. Le rafraîchissement du tiret se fait de multiples façons, mais les principales actions incluent toucher le sol, collecter des cristaux d\'endurance et les transitions d\'écran.' => 'The Prologue introduces players to the basic controls, such as jumping and the ability to climb walls. The dash—one of the main movement options in the game—is gifted to the player at the end of the level, seemingly by the Bird. This dash is the core mechanic of Celeste\'s gameplay, giving the player a short burst forward in the direction that the player is holding. It can only be used a limited number of times, and must be refreshed before dashing again. The status of the player\'s available dashes is indicated by Madeline\'s hair colour. Refreshing the dash is done in a multitude of ways, but the main actions include touching the ground, collecting stamina crystals and screen transitions.',
+    'Les sous-niveaux, et par extension les chapitres du jeu, deviennent progressivement plus difficiles à mesure que de nouvelles mécaniques et obstacles sont introduits, et l\'utilisation de ces éléments fusionne et s\'harmonise à mesure que le joueur se rapproche du sommet. Vers la fin du jeu, Celeste introduit de nombreux autres mouvements qui utilisent diverses combinaisons des trois mouvements de base (sauter, grimper et courir) en tandem; ces techniques sont explorées en détail sur la page Moves.' => 'The sublevels, and by extension the game\'s Chapters, get progressively harder as more mechanics and obstacles are introduced, and the use of these elements merge and flow together the closer the player gets to the summit. Towards the endgame, Celeste introduces many more moves that use various combinations of the basic three moves (jumping, climbing, and dashing) in tandem; these techniques are explored in detail on the Moves page.',
+];
+
+?>
