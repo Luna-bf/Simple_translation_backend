c0e6eb3c662452c027ac20134aa58cdec3d0be79
Luna-bf <lburattifayolle@gmail.com>
2024-11-15 10:01:00 +0100

    Update lang.php

--- languages/lang.php
+++ languages/lang.php
@@ -1,8 +1,9 @@
 <?php
 
-#Pour éviter d'avoir des problèmes si une session a déjà étée ouverte
+#Le if() va me permettre de savoir si une session est déjà ouverte ou non et donc, dans les deux cas, m'éviter d'avoir des erreurs.
+#Si une session est déjà ouverte, on ignore cette condition et on exécute le reste du code.
 if(session_status() === PHP_SESSION_NONE) {
-    session_start();
+    session_start(); #Si aucune session n'est ouverte, on en ouvre une nouvelle et on exécute le reste du code.
 }
 
 require getLanguageFile();
