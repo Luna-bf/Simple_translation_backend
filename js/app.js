21d24d9d8f9fb2e53ac20bb052ee3c9c5c21e9b9
Luna-bf <lburattifayolle@gmail.com>
2025-01-31 15:43:42 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 const dropdowns = document.querySelectorAll('.dropdown');
-        console.log(dropdowns);
+console.log(dropdowns);
         
-        for(let i = 0; i < dropdowns.length; i++) {
+for(let i = 0; i < dropdowns.length; i++) {
             
-            dropdowns[i].addEventListener('click', function(e) {
+    dropdowns[i].addEventListener('click', function(e) {
                 
-                //for(let x = 0; i < dropdowns.length; x++) {
-                  //  dropdowns[x].querySelector('.dropdown-content').classList.add('hide');
-                //}
-                e.currentTarget.querySelector('.dropdown-content').classList.toggle('hide');
-            });
-        }
+    //for(let x = 0; i < dropdowns.length; x++) {
+    //dropdowns[x].querySelector('.dropdown-content').classList.add('hide');
+    //}
+    e.currentTarget.querySelector('.dropdown-content').classList.toggle('hide');
+    });
+}
