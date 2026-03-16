bea8852fe771a5b0293ad4224530246465b6c56e
Luna-bf <lburattifayolle@gmail.com>
2026-03-16 17:06:54 +0100

    Ajout de div pour séparer les articles

--- templates/page.php
+++ templates/page.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,13 +8,14 @@
     <link rel="stylesheet" href="../public/css/style.css" type="text/css" />
     <link rel="icon" type="image/x-icon" href="../public/favicon/favicon.png" />
 </head>
+
 <body>
     <header>
         <div class="dropdown">
             <div class="dropdown-content hide">
-                <div><a href="#"><?= __('Profil')?></a></div>
-                <div><a href="#"><?= __('Paramètres')?></a></div>
-                <div><a href="#"><?= __('Se déconnecter')?></a></div>
+                <div><a href="#"><?= __('Profil') ?></a></div>
+                <div><a href="#"><?= __('Paramètres') ?></a></div>
+                <div><a href="#"><?= __('Se déconnecter') ?></a></div>
             </div>
         </div>
         <div class="dropdown-menu">
@@ -25,16 +27,21 @@
             </div>
         </div>
     </header>
-    
+
     <section>
-        <h1><?= __('Celeste, c\'est quoi ?')?></h1>
-        <p><?= __('Celeste est un jeu de plateforme acclamé par la critique créé par Extremely OK Games. Le jeu est sorti le 25 janvier 2018 sur Nintendo Switch, PlayStation 4, Xbox One, PC, Mac et Linux. Le jeu suit une jeune femme nommée Madeline alors qu\'elle escalade la montagne quelque peu magique de Celeste, faisant face à l\'opposition des obstacles naturels de la montagne et d\'elle-même.')?></p>
-        <h2><?= __('Gameplay')?></h2>
-        <p><?= __('Le gameplay de Celeste propose des commandes rapides et précises le long d\'une série de sous-niveaux de plateformes difficiles qui constituent un chapitre plus vaste . Il suit les commandes simples à apprendre et difficiles à maîtriser qui sont un élément de base de son genre, et le gameplay a souvent été comparé à d\'autres, comme Super Meat Boy.')?></p>
-        <p><?= __('Le prologue présente aux joueurs les commandes de base, comme le saut et la capacité à grimper aux murs. Le tiret, l\'une des principales options de mouvement du jeu, est offert au joueur à la fin du niveau, apparemment par l\'Oiseau . Ce tiret est la mécanique de base du gameplay de Celeste, offrant au joueur une courte poussée vers l\'avant dans la direction que le joueur tient. Il ne peut être utilisé qu\'un nombre limité de fois et doit être rafraîchi avant de se lancer à nouveau. L\'état des tirets disponibles du joueur est indiqué par la couleur des cheveux de Madeline. Le rafraîchissement du tiret se fait de multiples façons, mais les principales actions incluent toucher le sol, collecter des cristaux d\'endurance et les transitions d\'écran.')?></p>
-        <p><?= __('Les sous-niveaux, et par extension les chapitres du jeu, deviennent progressivement plus difficiles à mesure que de nouvelles mécaniques et obstacles sont introduits, et l\'utilisation de ces éléments fusionne et s\'harmonise à mesure que le joueur se rapproche du sommet. Vers la fin du jeu, Celeste introduit de nombreux autres mouvements qui utilisent diverses combinaisons des trois mouvements de base (sauter, grimper et courir) en tandem; ces techniques sont explorées en détail sur la page Moves.')?></p>
+        <div class="first-article">
+            <h1><?= __('Celeste, c\'est quoi ?') ?></h1>
+            <p><?= __('Celeste est un jeu de plateforme acclamé par la critique créé par Extremely OK Games. Le jeu est sorti le 25 janvier 2018 sur Nintendo Switch, PlayStation 4, Xbox One, PC, Mac et Linux. Le jeu suit une jeune femme nommée Madeline alors qu\'elle escalade la montagne quelque peu magique de Celeste, faisant face à l\'opposition des obstacles naturels de la montagne et d\'elle-même.') ?></p>
+        </div>
+        <div class="second-article">
+            <h2><?= __('Gameplay') ?></h2>
+            <p><?= __('Le gameplay de Celeste propose des commandes rapides et précises le long d\'une série de sous-niveaux de plateformes difficiles qui constituent un chapitre plus vaste . Il suit les commandes simples à apprendre et difficiles à maîtriser qui sont un élément de base de son genre, et le gameplay a souvent été comparé à d\'autres, comme Super Meat Boy.') ?></p>
+            <p><?= __('Le prologue présente aux joueurs les commandes de base, comme le saut et la capacité à grimper aux murs. Le tiret, l\'une des principales options de mouvement du jeu, est offert au joueur à la fin du niveau, apparemment par l\'Oiseau . Ce tiret est la mécanique de base du gameplay de Celeste, offrant au joueur une courte poussée vers l\'avant dans la direction que le joueur tient. Il ne peut être utilisé qu\'un nombre limité de fois et doit être rafraîchi avant de se lancer à nouveau. L\'état des tirets disponibles du joueur est indiqué par la couleur des cheveux de Madeline. Le rafraîchissement du tiret se fait de multiples façons, mais les principales actions incluent toucher le sol, collecter des cristaux d\'endurance et les transitions d\'écran.') ?></p>
+            <p><?= __('Les sous-niveaux, et par extension les chapitres du jeu, deviennent progressivement plus difficiles à mesure que de nouvelles mécaniques et obstacles sont introduits, et l\'utilisation de ces éléments fusionne et s\'harmonise à mesure que le joueur se rapproche du sommet. Vers la fin du jeu, Celeste introduit de nombreux autres mouvements qui utilisent diverses combinaisons des trois mouvements de base (sauter, grimper et courir) en tandem; ces techniques sont explorées en détail sur la page Moves.') ?></p>
+        </div>
     </section>
 </body>
 
-    <script type="text/javascript" src="../public/js/app.js"></script>
+<script type="text/javascript" src="../public/js/app.js"></script>
+
 </html>
